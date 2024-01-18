c1cea93871c049fbef45e10805ea72480bab8bda
박동윤 <ehddbs7458@gmail.com>
2024-01-18 22:08:40 +0900

    Init project

---
+++ src/test/java/BBOJK/sideprojectplatformbackend/SideProjectPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package BBOJK.sideprojectplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideProjectPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
