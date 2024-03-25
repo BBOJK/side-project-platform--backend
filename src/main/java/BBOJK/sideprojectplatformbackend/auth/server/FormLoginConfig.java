a6213c4d56fc4b5147143b1c7cbb20772589c3f1
박동윤 <ehddbs7458@gmail.com>
2024-03-25 22:26:32 +0900

    feat: add temporary UserDetailsService bean to FormLoginConfig.java

--- src/main/java/BBOJK/sideprojectplatformbackend/auth/server/FormLoginConfig.java
+++ src/main/java/BBOJK/sideprojectplatformbackend/auth/server/FormLoginConfig.java
@@ -2,13 +2,27 @@ package bbojk.sideprojectplatformbackend.auth.server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class FormLoginConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode("password");
+        UserDetails userDetails = User.withUsername("test")
+                .password(encodedPassword)
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
